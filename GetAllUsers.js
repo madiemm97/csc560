8caf8c897dc6c40a10419e8d861c123a42a3bc6b
Madie Martens <madiemm97@gmail.com>
2020-11-08 15:03:44 -0600

    first commit, server

---
+++ GetAllUsers.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express(); 
+var fs = require("fs"); 
+
+app.get('/getAllUsers', function (req, res) {
+    // First read existing userss
+    fs.readFile("users.json", 'utf8', function (err, data) {
+        console.log(data); 
+        res.end( data );
+    });
+}); 
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+    //  http://127.0.0.1:8081/GetAllUsers 
+})
